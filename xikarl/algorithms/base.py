54d423e9efdc3f1a57c66906c95d77e2006763f8
xikasan <xika@xika.jp>
2019-11-06 23:23:15 +0900

    [feat] add action observation space size getter

--- xikarl/algorithms/base.py
+++ xikarl/algorithms/base.py
@@ -11,3 +11,18 @@ class DefaultAlgorithm:
     def __init__(self, name="DefaultAlgo"):
         self.name = name
 
+    @staticmethod
+    def get_obs_act_size(env):
+        return (
+            len(env.observation_space.high),
+            len(env.action_space.high)
+        )
+
+    @staticmethod
+    def obs_size(env):
+        return len(env.observation_space.high)
+
+    @staticmethod
+    def act_size(env):
+        return len(env.action_space.high)
+
